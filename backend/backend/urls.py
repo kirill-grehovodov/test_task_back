efcd3d00c7eef3bbb97cec822207580998ac337d
Grekhovodov Kirill <rikilll666@gmail.com>
2021-03-22 19:19:17 +0600

    Создал главную страницу, где пока выводятся все посты

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('src.profiles.urls')),
+    path('', include('src.webapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/src/webapp/views.py
+++ backend/src/webapp/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+
+from src.webapp.models import Post
+
+
+class IndexView(ListView):
+    template_name = 'index.html'
+    model = Post
+    context_object_name = 'posts'
+    paginate_by = 2
+
+    def get(self, request, *args, **kwargs):
+        # self.form = self.get_search_form()
+        # self.search_value = self.get_search_value()
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        # if self.request.user.groups.filter(name__in=['Team Lead', 'Project Manager']):
+        #     context['user_list_perm'] = True
+        # context['form'] = self.form
+        # if self.search_value:
+        #     context['query'] = urlencode({'search': self.search_value})
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        # if self.search_value:
+        #     query = Q(name__icontains=self.search_value) | Q(description__icontains=self.search_value)
+        #     queryset = queryset.filter(query)
+        return queryset
+
+    # def get_search_form(self):
+    #     return SearchForm(self.request.GET)
+    #
+    # def get_search_value(self):
+    #     if self.form.is_valid():
+    #         return self.form.cleaned_data['search']
+    #     return None
